a33ac8a1e3e6b8bae4e8501c28f5b4583b738771
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2023-07-19 12:41:16 +0900

    고유명사 사전관리 기능 확장

--- R/dictonary.R
+++ R/dictonary.R
@@ -8,6 +8,7 @@
 #' \itemize{
 #' \item 인명사전 : person.csv
 #' \item 지명사전 : place.csv
+#' \item 고유명사사전 : nnp.csv
 #' }
 #' 고유명사 사전의 경우에는 타입, 첫번째 품사, 마지막 품사, 원형, 인텍스표현의 정보는 의미가 없어 모두 *로 표현함.
 #' @return spec_tbl_df. 고유명사 사전 정의를 담은 tibble 객체.
@@ -34,7 +35,7 @@
 #' @importFrom glue glue
 #' @importFrom readr read_csv
 #' @export
-get_userdic_nnp <- function(type = c("person", "place"), userdic_path = NULL) {
+get_userdic_nnp <- function(type = c("person", "place", "nnp"), userdic_path = NULL) {
   type <- match.arg(type)
   
   if (is_windows()) {
@@ -60,32 +61,42 @@ get_userdic_nnp <- function(type = c("person", "place"), userdic_path = NULL) {
 
 
 #' Write to the user-defined person dictionary file.
-#' @description 사용자 사전 중 고유명사 사전에 등록하기 위해 인명/지명을 인명/지명 사전 파일에 추가
+#' @description 사용자 사전 중 고유명사 사전에 등록하기 위해 인명/지명을 인명/지명/고유명사 사전 파일에 추가
 #' @param x character. 고유명사 사전에 등록할 이름들.
-#' @param type character. 인명사전과 지명사전에서 등록할 사용자 정의 고유명사 사전 선택.
+#' @param type character. 인명사전과 지명사전, 고유명사 사전에서 등록할 사용자 정의 고유명사 사전 선택.
 #' @param userdic_path character. 사용자 정의 고유명사 사전 파일이 존재하는 경로.
 #' 지정하지 않으면 사전이 설치된 기본 경로에서 파일을 읽어온다.
-#' @details 사용자 사전정의 디렉토리의 person.csv/place.csv 파일에 등록할 인명/지명을 추가한다. 
+#' @details 사용자 사전정의 디렉토리의 person.csv/place.csv/nnp.csv 파일에 등록할 인명/지명/고유명사를 추가한다. 
 #' @examples
 #' \dontrun{
+#' # 인명 사전
 #' get_userdic_nnp()
 #' append_userdic_nnp(c("변학도"))
+#' 
+#' # 지명 사전
+#' get_userdic_nnp("place")
 #' append_userdic_nnp(c("영귀미면"), "place")
-#' get_userdic_nnp()
+#' get_userdic_nnp("place")
+#' 
+#' # 기타 고유명사 사전  
+#' get_userdic_nnp("nnp")
+#' append_userdic_nnp(c("릴리움"), "nnp")
+#' get_userdic_nnp("nnp")
 #' }
 #' @import dplyr
 #' @importFrom glue glue
 #' @importFrom readr write_csv
 #' @importFrom purrr map_lgl
 #' @importFrom rstudioapi askForPassword
-#' @importFrom cli cli_rule cli_alert_success
+#' @importFrom cli cli_rule cli_alert_success cli_alert_warning
 #' @export
-append_userdic_nnp <- function(x, type = c("person", "place"), userdic_path = NULL) {
+append_userdic_nnp <- function(x, type = c("person", "place", "nnp"), userdic_path = NULL) {
   type <- match.arg(type)
   
   type_name <- case_when(
     type %in% "person" ~ "인명",
     type %in% "place"  ~ "지명",
+    type %in% "nnp"    ~ "*",    
     TRUE ~               "인명"
   )
   
@@ -131,8 +142,11 @@ append_userdic_nnp <- function(x, type = c("person", "place"), userdic_path = NU
   meta <- get_userdic_nnp(type, userdic_path)
   nnps <- setdiff(x, meta$표층형)
   
+  type_name2 <- ifelse(type_name %in% "*", "고유명사", type_name)
+  
   if (length(nnps) == 0) {
-    stop(glue::glue("등록할 {type_name}이 없거나 이미 모두 등록되어 있을 수도 있습니다."))
+    cli::cli_alert_warning(glue::glue("등록할 {type_name2}(이/가) 없거나 이미 모두 등록되어 있을 수도 있습니다."))
+    return(NULL)
   }
   
   final_consonants <- nnps %>% 
@@ -164,9 +178,9 @@ append_userdic_nnp <- function(x, type = c("person", "place"), userdic_path = NU
   
   n_add_nnps <- length(nnps)  
   
-  cli::cli_rule(glue::glue("사전 파일에 {type_name} 추가하기"))
+  cli::cli_rule(glue::glue("사전 파일에 {type_name2} 추가하기"))
   cli::cli_alert_success(c("신규 추가 건수: {n_add_nnps}"))
-  cli::cli_alert_success(c("최종 {type_name} 건수: {NROW(df_nnps)}"))  
+  cli::cli_alert_success(c("최종 {type_name2} 건수: {NROW(df_nnps)}"))  
 }
 
 
